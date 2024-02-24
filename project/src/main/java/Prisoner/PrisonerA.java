c7f7bebbeb40ff6456a7c135a14f159ee2457c3a
Julius Oexle <julius.oexle@gmx.de>
2024-02-24 16:16:22 +0100

    test branch

--- project/src/main/java/Prisoner/PrisonerA.java
+++ project/src/main/java/Prisoner/PrisonerA.java
@@ -2,18 +2,18 @@ package Prisoner;
 
 import java.util.ArrayList;
 
-public class PrisonerX extends Prisoner {
+public class PrisonerA extends Prisoner {
 
     //TODO set your team name
-    public PrisonerX() {
-        teamName = "";
+    public PrisonerA() {
+        teamName = "The Informätics";
     }
 
 
     //TODO implement the decision making process
     public Boolean makeDecision(ArrayList<Boolean> myDecisions, ArrayList<Boolean> opponentDecisions) {
 
-        return false;
+        return myDecisions.get(myDecisions.size() - 1);
     }
 
     //TODO if you need to reset any variables at the end of each game, do so here
